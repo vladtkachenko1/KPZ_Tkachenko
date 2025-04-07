4e52b919a436dd87e15323b63fff78e51db3a55e
Vlad Tkachenko <ipz232_tvo@student.ztu.edu.ua>
2025-04-08 01:58:26 +0300

    Створення гловного методу програми, який демонструє правильність коду

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,1 +1,52 @@
-﻿
+﻿using System;
+using AbstractFactory.Factories;
+using AbstractFactory.Devices;
+using AbstractFactory;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Вітаємо в системі створення девайсів!\n");
+
+        DeviceFactory iProneFactory = new IProneFactory();
+        DeviceFactory kiaomiFactory = new KiaomiFactory();
+        DeviceFactory balaxyFactory = new BalaxyFactory();
+
+        AbstractDevice iProneLaptop = iProneFactory.CreateLaptop();
+        AbstractDevice iProneSmartphone = iProneFactory.CreateSmartphone();
+        AbstractDevice iProneNetbook = iProneFactory.CreateNetbook();
+        AbstractDevice iProneEBook = iProneFactory.CreateEBook();
+
+        AbstractDevice kiaomiLaptop = kiaomiFactory.CreateLaptop();
+        AbstractDevice kiaomiSmartphone = kiaomiFactory.CreateSmartphone();
+        AbstractDevice kiaomiNetbook = kiaomiFactory.CreateNetbook();
+        AbstractDevice kiaomiEBook = kiaomiFactory.CreateEBook();
+
+        AbstractDevice balaxyLaptop = balaxyFactory.CreateLaptop();
+        AbstractDevice balaxySmartphone = balaxyFactory.CreateSmartphone();
+        AbstractDevice balaxyNetbook = balaxyFactory.CreateNetbook();
+        AbstractDevice balaxyEBook = balaxyFactory.CreateEBook();
+
+        Console.WriteLine("Продукти від бренду IProne:");
+        iProneLaptop.ShowInfo();
+        iProneSmartphone.ShowInfo();
+        iProneNetbook.ShowInfo();
+        iProneEBook.ShowInfo();
+
+        Console.WriteLine("\nПродукти від бренду Kiaomi:");
+        kiaomiLaptop.ShowInfo();
+        kiaomiSmartphone.ShowInfo();
+        kiaomiNetbook.ShowInfo();
+        kiaomiEBook.ShowInfo();
+
+        Console.WriteLine("\nПродукти від бренду Balaxy:");
+        balaxyLaptop.ShowInfo();
+        balaxySmartphone.ShowInfo();
+        balaxyNetbook.ShowInfo();
+        balaxyEBook.ShowInfo();
+
+        Console.ReadLine();
+    }
+}
--- AbstractFactory/obj/Debug/net8.0/AbstractFactory.AssemblyInfo.cs
+++ AbstractFactory/obj/Debug/net8.0/AbstractFactory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b3befd7aec36868cd8aba5e05d175f147f55df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea3e140e2a5fbe8c22665ee6d76d9bb63d95e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
