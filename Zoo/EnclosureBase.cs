39e15cd55bc64bdf8ab327ae4670b8c25e348ebf
Vlad Tkachenko <ipz232_tvo@student.ztu.edu.ua>
2025-04-07 14:56:35 +0300

    Створено абстрактний клас EnclosureBase для вольєрів

---
+++ Zoo/EnclosureBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    abstract class EnclosureBase
+    {
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public List<Animal> Animals { get; set; } = new List<Animal>();
+
+        public EnclosureBase(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+        }
+
+        public virtual void AddAnimal(Animal animal)
+        {
+            if (Animals.Count < Capacity)
+            {
+                Animals.Add(animal);
+                Console.WriteLine($"{animal.Name} додано у {Type} вольєр.");
+            }
+            else
+            {
+                Console.WriteLine($"Вольєр {Type} переповнений!");
+            }
+        }
+
+        public abstract void DisplayDetails();
+    }
+}
