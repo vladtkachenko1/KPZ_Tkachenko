52df6a6d4afb5c4d111052b1dbcb0e748294e3fb
Vlad Tkachenko <ipz232_tvo@student.ztu.edu.ua>
2025-04-07 14:19:02 +0300

    Додано абстрактний клас Animal для представлення тварин

---
+++ Zoo/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+// Абстрактний клас для тварин (S - Single Responsibility)
+abstract class Animal
+{
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public string Diet { get; set; }
+
+    public Animal(string name, string species, string diet)
+    {
+        Name = name;
+        Species = species;
+        Diet = diet;
+    }
+
+    public abstract void MakeSound();
+}
+}
