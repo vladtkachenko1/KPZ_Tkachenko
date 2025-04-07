c8251186aaa99773d4f3c127fb048aa55995f25e
Vlad Tkachenko <ipz232_tvo@student.ztu.edu.ua>
2025-04-08 01:32:48 +0300

    Завдання 2. Створено абстрактний клас AbstractDevice, та інтерфейс IDeviceFactory

---
+++ AbstractFactory/AbstractDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public abstract class AbstractDevice
+    {
+        public abstract string GetDeviceType();
+        public abstract string GetBrand();
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Тип пристрою: {GetDeviceType()} | Бренд: {GetBrand()}");
+        }
+    }
+}
---
+++ AbstractFactory/IDeviceFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public interface IDeviceFactory
+    {
+        AbstractDevice CreateLaptop();
+        AbstractDevice CreateNetbook();
+        AbstractDevice CreateEBook();
+        AbstractDevice CreateSmartphone();
+    }
+}
