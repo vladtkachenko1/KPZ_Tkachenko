bd6fa5882deb04c0abacfa355f63da2092feab41
Vlad Tkachenko <ipz232_tvo@student.ztu.edu.ua>
2025-04-08 02:01:17 +0300

    Завдання 2.Рефакторинг. Видалення зайвого інтерфейсу

--- AbstractFactory/obj/Debug/net8.0/AbstractFactory.AssemblyInfo.cs
+++ AbstractFactory/obj/Debug/net8.0/AbstractFactory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea3e140e2a5fbe8c22665ee6d76d9bb63d95e04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e52b919a436dd87e15323b63fff78e51db3a55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbstractFactory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
