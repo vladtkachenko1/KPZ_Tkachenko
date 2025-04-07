100269b742e412efa2b3699c89b4977cf8139566
Vlad Tkachenko <ipz232_tvo@student.ztu.edu.ua>
2025-04-07 14:28:33 +0300

    Додано клас Enclosure для представлення вольєрів у зоопарку

--- Zoo/obj/Debug/net8.0/Zoo.AssemblyInfo.cs
+++ Zoo/obj/Debug/net8.0/Zoo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3e4ab25a80e4ad94167cdf36ae656f6eb0594d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7926d5e54fd24f5a000e3b12b445ac375d2b5708")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
