f4bd7e70710627658b733a91fec6de712877e156
Vlad Tkachenko <ipz232_tvo@student.ztu.edu.ua>
2025-04-07 23:57:09 +0300

    Створено абстрактний клас Subscription

---
+++ FactoryMetрod/Subscription.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMetрod
+{
+    public abstract class Subscription
+    {
+        public abstract decimal MonthlyFee { get; }
+        public abstract int MinPeriodMonths { get; }
+        public abstract List<string> Channels { get; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Subscription: {GetType().Name}");
+            Console.WriteLine($"Monthly Fee: {MonthlyFee:C}");
+            Console.WriteLine($"Min Period: {MinPeriodMonths} months");
+            Console.WriteLine("Channels: " + string.Join(", ", Channels));
+            Console.WriteLine();
+        }
+    }
+
+}
