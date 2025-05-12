beb54ad209b248e82d2adcb8ad51c7843f5a2ee6
Vlad Tkachenko <ipz232_tvo@student.ztu.edu.ua>
2025-05-12 15:29:08 +0300

    Реалізовано перший шаблон для проєкту 'Шаблонний метод'. Створено клас LifecycleElement, який визначає структуру життєвого циклу HTML-подібного елемента. Клас CustomDivElement перевизначає життєві етапи для демонстрації кастомної поведінки.

---
+++ Composer/CustomDivElement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composer
+{
+    public class CustomDivElement : LifecycleElement
+    {
+        public CustomDivElement() : base("div") { }
+
+        protected override void OnCreated()
+        {
+            Console.WriteLine("CustomDivElement: OnCreated");
+        }
+
+        protected override void OnInserted()
+        {
+            Console.WriteLine("CustomDivElement: OnInserted");
+        }
+
+        protected override void OnStylesApplied()
+        {
+            Console.WriteLine("CustomDivElement: OnStylesApplied");
+        }
+
+        protected override void OnClassListApplied()
+        {
+            Console.WriteLine("CustomDivElement: OnClassListApplied");
+        }
+
+        protected override void OnTextRendered()
+        {
+            Console.WriteLine("CustomDivElement: OnTextRendered");
+        }
+    }
+
+}
